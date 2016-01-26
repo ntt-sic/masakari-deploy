97f070bf529a6a00ce14e704b3aee8091f905c43
Tyler Britten <me@vmtyler.com>
2016-01-26 11:02:11 -0500

    Updated Recipe

--- cookbooks/masakari_compute_conf/recipes/default.rb
+++ cookbooks/masakari_compute_conf/recipes/default.rb
@@ -1,41 +1,39 @@
 case node[:platform]
 when 'ubuntu', 'debian'
-  dpkg_package "hostmonitor_1.0.0-1_all" do
-    source '/home/stack/masakari/hostmonitor_1.0.0-1_all.deb'
+  dpkg_package "masakari-hostmonitor_1.0.0-1_all" do
+    source '/home/stack/masakari/masakari-hostmonitor_1.0.0-1_all.deb'
     action :install
   end
 
-  dpkg_package "instancemonitor_1.0.0-1_all" do
-    source '/home/stack/masakari/instancemonitor_1.0.0-1_all.deb'
+  dpkg_package "masakari-instancemonitor_1.0.0-1_all" do
+    source '/home/stack/masakari/masakari-instancemonitor_1.0.0-1_all.deb'
     action :install
   end
 
-  dpkg_package "processmonitor_1.0.0-1_all" do
-    source '/home/stack/masakari/processmonitor_1.0.0-1_all.deb'
+  dpkg_package "masakari-processmonitor_1.0.0-1_all" do
+    source '/home/stack/masakari/masakari-processmonitor_1.0.0-1_all.deb'
     action :install
   end
 when 'redhat', 'centos'
   #TBD
-  directory '/etc/hostmonitor' # fake
-  directory '/etc/instancemonitor' # fake
-  directory '/etc/processmonitor' # fake
+  directory '/etc/masakari' # fake
 end
 
-template '/etc/hostmonitor/hostmonitor.conf' do
+template '/etc/masakari/masakari-hostmonitor.conf' do
   source 'hostmonitor.conf.erb'
   owner 'root'
   group 'root'
   mode '0644'
 end
 
-template '/etc/instancemonitor/instancemonitor.conf' do
+template '/etc/masakari/masakari-instancemonitor.conf' do
   source 'instancemonitor.conf.erb'
   owner 'root'
   group 'root'
   mode '0644'
 end
 
-template '/etc/processmonitor/processmonitor.conf' do
+template '/etc/masakari/masakari-processmonitor.conf' do
   source 'processmonitor.conf.erb'
   owner 'root'
   group 'root'
